c12840b9c9671de75bd878ce5ffafff3038b6c4f
L11K <L11K@users.noreply.github.com>
2018-09-04 13:17:19 -0300

    Resurrección: Segunda Etapa

--- main.py
+++ main.py
@@ -159,8 +159,12 @@ class StdOutListener(StreamListener):
                 if match:
                     webhook = Webhook.partial(match.group("id"), match.group("token"), adapter=RequestsWebhookAdapter())
                     try:
-                        if "#HashTagTest" in text:
-                            webhook.send(embed=embed)
+                        if data_discord["Filtered_Text"]: # if Filtered_Text is present do the filtering
+                          if any(Filtered_Text in text for Filtered_Text in data_discord["Filtered_Text"]):
+                              webhook.send(embed=embed)
+                              
+                              else: # Filtered_Text was not present, always post.
+                               webhook.send(embed=embed)
                     except discord.errors.HTTPException as error:
                         print('---------Error---------')
                         print('discord.errors.HTTPException')
